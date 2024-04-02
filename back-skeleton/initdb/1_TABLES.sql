e65b28271f1378258d742d5b29ce273e77bb5447
philippeB <philippe.beaulieu@edu.ece.fr>
2024-04-02 15:57:52 +0200

    main

--- back-skeleton/initdb/1_TABLES.sql
+++ back-skeleton/initdb/1_TABLES.sql
@@ -10,62 +10,62 @@ DROP TABLE IF EXISTS Utilisateur CASCADE;
 -- Création de la table Film
 
 CREATE TABLE Film (
-      Id_Film SERIAL PRIMARY KEY,
-      Titre VARCHAR(50),
-      Id_Realisateur INT,
-      Synopsis VARCHAR(255),
-      Date DATE,
-      Duree FLOAT,
-      Image bytea,
-      Genre VARCHAR(50)
+                      Id_Film SERIAL PRIMARY KEY,
+                      Titre VARCHAR(50),
+                      Id_Realisateur INT,
+                      Synopsis VARCHAR(255),
+                      Date DATE,
+                      Duree FLOAT,
+                      Image bytea,
+                      Genre VARCHAR(50)
 );
 
 -- Création de la table Réalisateurs
 -- Création de la table Réalisateurs avec la colonne Id_Realisateur définie comme SERIAL pour l'auto-incrémentation
 CREATE TABLE Realisateurs (
-      Id_Realisateur SERIAL PRIMARY KEY,
-      Nom VARCHAR(50),
-      Prenom VARCHAR(50),
-      Biographie VARCHAR(255),
-      Date DATE,
-      Nationalite VARCHAR(50)
+                              Id_Realisateur SERIAL PRIMARY KEY,
+                              Nom VARCHAR(50),
+                              Prenom VARCHAR(50),
+                              Biographie VARCHAR(255),
+                              Date DATE,
+                              Nationalite VARCHAR(50)
 );
 
 
 -- Création de la table Salle
 CREATE TABLE Salle (
-   Id_Salle SERIAL PRIMARY KEY,
-   Capacite INT,
-   Equipement VARCHAR(50),
-   Id_Film INT,
-   FOREIGN KEY (Id_Film) REFERENCES Film(Id_Film)
+                       Id_Salle SERIAL PRIMARY KEY,
+                       Capacite INT,
+                       Equipement VARCHAR(50),
+                       Id_Film INT,
+                       FOREIGN KEY (Id_Film) REFERENCES Film(Id_Film)
 );
 
 -- Création de la table Seance
 CREATE TABLE Seance (
-    Id_Seance SERIAL PRIMARY KEY,
-    Id_Film INT,
-    Id_Salle INT,
-    Date_Heure DATE,
-    Prix FLOAT,
-    FOREIGN KEY (Id_Film) REFERENCES Film(Id_Film),
-    FOREIGN KEY (Id_Salle) REFERENCES Salle(Id_Salle)
+                        Id_Seance SERIAL PRIMARY KEY,
+                        Id_Film INT,
+                        Id_Salle INT,
+                        Date_Heure DATE,
+                        Prix FLOAT,
+                        FOREIGN KEY (Id_Film) REFERENCES Film(Id_Film),
+                        FOREIGN KEY (Id_Salle) REFERENCES Salle(Id_Salle)
 );
 
 -- Création de la table Utilisateur
 CREATE TABLE Utilisateur (
-     Id_User SERIAL PRIMARY KEY,
-     Nom VARCHAR(50),
-     Prenom VARCHAR(50),
-     Mail VARCHAR(50),
-     Mdp VARCHAR(50)
+                             Id_User SERIAL PRIMARY KEY,
+                             Nom VARCHAR(50),
+                             Prenom VARCHAR(50),
+                             Mail VARCHAR(50),
+                             Mdp VARCHAR(50)
 );
 
 -- Création de la table Reservation
 CREATE TABLE Reservation (
-     Id_Reservation SERIAL PRIMARY KEY,
-     Id_User INT,
-     Id_Seance INT,
-     FOREIGN KEY (Id_User) REFERENCES Utilisateur(Id_User),
-     FOREIGN KEY (Id_Seance) REFERENCES Seance(Id_Seance)
+                             Id_Reservation SERIAL PRIMARY KEY,
+                             Id_User INT,
+                             Id_Seance INT,
+                             FOREIGN KEY (Id_User) REFERENCES Utilisateur(Id_User),
+                             FOREIGN KEY (Id_Seance) REFERENCES Seance(Id_Seance)
 );
